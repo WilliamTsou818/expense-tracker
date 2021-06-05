08183314c8844a4639a553f9d8c23b78b49befce
WilliamTsou818 <cccc1589@gmail.com>
2021-06-05 15:28:34 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -2,27 +2,15 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
+
 
 const port = 3000
+
+require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-mongoose.connect('mongodb://localhost/expense-tracker', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongoDB error!')
-})
-
-db.once('open', () => {
-  console.log('mongoDB connected!')
-})
 
 app.get('/', (req, res) => {
   res.render('index')
