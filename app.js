f80f56ceab9d14867e817db28aabb7ff58d24171
WilliamTsou818 <cccc1589@gmail.com>
2021-11-11 17:41:56 +0800

    feat: add REDIS_URL environment variable for heroku redis server

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ require('./config/mongoose')
 
 // include redis and set redis config
 const redis = require('redis')
-const redisClient = redis.createClient()
+const redisUrl = process.env.REDIS_URL || ''
+const redisClient = redis.createClient(redisUrl)
 const connectRedis = require('connect-redis')
 const RedisStore = connectRedis(session)
 
